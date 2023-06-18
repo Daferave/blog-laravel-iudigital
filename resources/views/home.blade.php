db2d2008b1d2b5c02766f46c6d9e1e59b23f2579
Daferave <94031932+Daferave@users.noreply.github.com>
2023-06-18 00:26:25 -0500

    Add files via upload

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,6 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
@@ -13,19 +12,10 @@
                             {{ session('status') }}
                         </div>
                     @endif
-                    <div class="card" style="width: 18rem;">
-                        <img src="..." class="card-img-top" alt="...">
-                        <div class="card-body">
-                          <h5 class="card-title">Módulo de Post</h5>
-                          <p class="card-text">Crear Publicaciones.</p>
-                          <a href="#" class="btn btn-primary">Ir al módulo</a>
-                        </div>
-                      </div>
-                      
-                    
+
+                    {{ __('You are logged in!') }}
                 </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
